1b3f2dd1706656ccc170933fb23fc740d44ac000
akashjaiswal09 <52067155+akashjaiswal09@users.noreply.github.com>
2019-07-03 15:34:27 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Akash',
+    author_email='akash_jaiswal09@yahoo.com',
+    description='HospitalManagementSystem for college python course'
+)
